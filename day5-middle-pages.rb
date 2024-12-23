cf8578a02ff826bb74b37c237bbd88f78bb2e162
Michał Lech <mlech@visa.com>
2024-12-23 19:31:27 +0000

    day 5 star 2

--- day5-middle-pages.rb
+++ day5-middle-pages.rb
@@ -1,10 +1,10 @@
-rules = {}
+$rules = {}
 # updates = []
 
 phase = :rules
 
-def update_compliant?(update, rules)
-  rules.all? do |predecessor, successors|
+def update_compliant?(update)
+  $rules.all? do |predecessor, successors|
     successors.all? do |succ|
       isucc = update.index(succ)
       ipred = update.index(predecessor)
@@ -13,18 +13,33 @@ def update_compliant?(update, rules)
   end
 end
 
+def compare_by_rules(a, b)
+  succs_of_a = $rules[a]
+  succs_of_b = $rules[b]
+  return -1 if succs_of_a&.include? b
+  return  1 if succs_of_b&.include? a
+  return 0
+end
+
+def apply_rules(update)
+  update.sort(&method(:compare_by_rules))
+end
+
 sum_of_mids = 0
 while next_line = STDIN.gets&.chomp do
   next phase = :updates if next_line == ""
   if phase == :rules
     predecessor, successor = next_line.split('|')
-    (rules[predecessor] ||= []) << successor
+    ($rules[predecessor] ||= []) << successor
     next
   end
   update = next_line.split(",")
   upd_len = update.length
   raise "Broken update #{next_line}" unless upd_len.odd?
-  next unless update_compliant?(update, rules)
+  # next unless update_compliant?(update, rules)
+  next if update_compliant?(update)
+  update = apply_rules(update)
+  raise "Can't sort!" unless update_compliant?(update)
   mid = update[upd_len / 2]
   sum_of_mids += mid.to_i
 end
