1aa33573346cce3dab694ac417921f0900d089f4
Michał Lech <mlech@visa.com>
2024-12-24 02:04:13 +0000

    day 9 star 2

--- d9-defrag.rb
+++ d9-defrag.rb
@@ -1,36 +1,52 @@
 diskmap = STDIN.gets.chomp
-blocks = []
+# blocks = []
 file_id = 0
 free_space = false
+free_blocks = []
+file_blocks = []
+offset = 0
 diskmap.each_char do |c|
+  len = c.to_i
   if free_space
-    blocks += ['.'] * c.to_i
+    free_blocks << [offset, len]
   else
-    blocks += [file_id] * c.to_i
+    file_blocks << [offset, len, file_id]
     file_id += 1
   end
+  offset += len
   free_space = !free_space
 end
 
-puts "#{blocks.length} blocks"
+puts "Blocks: #{free_blocks.length} free, #{file_blocks.length} used."
 
-i = 0
-blocks.pop while blocks.last == '.'
-end_pos = blocks.length - 1
-while true do
-  new_place = blocks.index '.'
-  break unless new_place
-  break unless new_place < end_pos
-  blocks[new_place] = blocks.pop
-  end_pos -= 1
-  i += 1
-  print "\33[2K\r", blocks.count('.') if i % 100 == 0
-  # puts blocks.map(&:to_s).join
+file_to_move_idx = file_blocks.length - 1
+while file_to_move_idx >= 0 do
+  file_to_move = file_blocks[file_to_move_idx]
+  file_offset, file_len, file_id = file_to_move
+
+  new_place = free_blocks.find do |free_offset, free_len|
+    free_len >= file_len
+  end
+
+  if new_place
+    free_offset, free_len = new_place
+    if free_offset < file_offset
+      file_to_move[0] = free_offset
+      if free_len > file_len
+        new_place[0] += file_len
+        new_place[1] -= file_len
+      else 
+        free_blocks.delete new_place
+      end        
+    end
+  end
+  file_to_move_idx -= 1
+  print "\33[2K\rFile #{file_id} checked." if file_to_move_idx % 100 == 0
 end
 
 checksum = 0
-blocks.each_with_index do |file_id, position|
-  checksum += file_id * position
+file_blocks.each do |offset, len, id|
+  checksum += (offset..offset + len - 1).map { |off| off * id }.reduce(:+)
 end
 puts
 puts checksum
